017c4f816045ad9baede9ac8b0f916ac85164a87
Jacob Pyke <s3755145@student.rmit.edu.au>
2020-11-18 19:30:44 +0100

    Added sudo to root commands
    Signed-off-by: Jacob Pyke <s3755145@student.rmit.edu.au>

--- control_cpu.sh
+++ control_cpu.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
+# Binary Variables
 ECHO="/bin/echo"
 TOP="/usr/bin/top"
 GREP="/bin/grep"
 AWK="/usr/bin/awk"
 
+# Used to stop the LED Triggering
 initialize () {
     # Turn off the LED Triggering from Disk Activity
-    $ECHO none > /sys/class/leds/led0/trigger
+    sudo su "${ECHO} none > /sys/class/leds/led0/trigger"
 }
 
+# Used to turn on the LED
 turn_on_led () {
-    $ECHO 1 > /sys/class/leds/led0/brightness
+    sudo su "${ECHO} 1 > /sys/class/leds/led0/brightness"
 }
 
+# Used to turn off the LED
 turn_off_led () {
-    $ECHO 0 > /sys/class/leds/led0/brightness
+    sudo su "${ECHO} 0 > /sys/class/leds/led0/brightness"
 }
 
+# Polls the CPU and writes the usage to $USAGE and the remainder of $USAGE to $NONUSAGE
 poll_cpu () {
     USAGE=`${TOP} -bn1 | ${GREP} 'Cpu(s)' | ${AWK} '{printf "%.0f\n", \$2}' | ${AWK} '{ print $1 / 100}'`
     NONUSAGE=`${TOP} -bn1 | ${GREP} 'Cpu(s)' | ${AWK} '{printf "%.0f\n", 100 - \$2}' | ${AWK} '{ print $1 / 100}'`
