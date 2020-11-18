81d03dc44803bc59b452c441daa2be4eb20487a1
Jacob Pyke <s3755145@student.rmit.edu.au>
2020-11-18 18:05:04 +0100

    Add CPU control script
    Signed-off-by: Jacob Pyke <s3755145@student.rmit.edu.au>

---
+++ control_cpu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ECHO="/bin/echo"
+
+initialize () {
+    # Turn off the LED Triggering from Disk Activity
+    $ECHO > /sys/class/leds/led0/trigger
+}
+
+turn_on_led () {
+    $ECHO 1 > /sys/class/leds/led0/brightness
+}
+
+turn_off_led () {
+    $ECHO 0 > /sys/class/leds/led0/brightness
+}
+
+poll_cpu () {
+    grep 'cpu ' /proc/stat | echo $1 
+}
+
+initialize
+poll_cpu
